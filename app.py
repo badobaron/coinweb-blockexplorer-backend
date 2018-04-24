26be1b82c4c995bdbcc81013f6c5c61dee6ca64d
hephest <hephaestum@hotmail.com>
2018-04-24 17:51:32 +0300

    Added assets category templates & routing

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
+# Flask basic imports for usage routing & rendering features
 from flask import Flask, render_template
 
+# Registering our app
 app = Flask(__name__)
 
-
+# Site root routing
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Assets category routing
 @app.route('/assets')
 def assets():
     return render_template('assets.html')
 
+@app.route('/assets/all')
+def assets_all():
+    return render_template('assets_all.html')
+
+@app.route('/assets/named')
+def assets_named():
+    return render_template('assets_named.html')
+
+@app.route('/assets/subassets')
+def assets_subassets():
+    return render_template('assets_subassets.html')
+
+@app.route('/assets/numeric')
+def assets_numeric():
+    return render_template('assets_numeric.html')
 
 if __name__ == '__main__':
     app.run()
