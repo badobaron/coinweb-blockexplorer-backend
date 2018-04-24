43991e0320b45d3ef23cd6594622014f01e9fe41
hephest <hephaestum@hotmail.com>
2018-04-24 14:43:50 +0300

    Added templates and static files

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    //Sidenav initialisation
+    $('.sidenav').sidenav();
+    //Dropdown initialisation
+    $(".dropdown-trigger").dropdown();
+    //Collapsible initialization
+    $('.collapsible').collapsible();
+})
