4b5ac375d22676ddb9dc1b2db6b1dc7dd81e1fb3
hephest <hephaestum@hotmail.com>
2018-04-25 10:52:03 +0300

    Added error handlers & basic templates

--- app.py
+++ app.py
@@ -4,6 +4,23 @@ from flask import Flask, render_template
 # Registering our app
 app = Flask(__name__)
 
+# Error handling section
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(403)
+def forbidden(e):
+    return render_template('403.html'), 403
+
+@app.errorhandler(410)
+def gone(e):
+    return render_template('410.html'), 410
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
+
 # Site root routing
 @app.route('/')
 def index():
